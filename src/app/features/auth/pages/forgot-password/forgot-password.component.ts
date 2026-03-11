8a8061a2cb5e01dea1983cfe58f09e645939308b
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2026-03-11 12:59:22 +0000

    feat: update forgot password component to use translation for title and subtitle

--- src/app/features/auth/pages/forgot-password/forgot-password.component.ts
+++ src/app/features/auth/pages/forgot-password/forgot-password.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.scss'
 })
