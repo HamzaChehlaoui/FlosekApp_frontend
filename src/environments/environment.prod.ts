d299eee78d4daf7837f9bf8f977864cb50cf56a1
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2026-03-26 10:22:13 +0000

    feat: update API URL for production and development environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.flosek.com/api'
+  apiUrl: 'https://flosek-latest.onrender.com/api'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081/api',
+  // apiUrl: 'http://localhost:8081/api',
+  apiUrl: 'https://flosek-latest.onrender.com/api' ,
   googleClientId: '23387237248-5u540b822vpaijn2127dse2dd2sah1fj.apps.googleusercontent.com'
 };
